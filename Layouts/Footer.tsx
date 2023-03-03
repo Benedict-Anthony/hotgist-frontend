e6903b7880012fd446b3425b45c5713a5a6ae7ff
Benedict Anthony <benedict.ughulu@gmail.com>
2023-03-03 09:02:21 -0800

    Update Footer.tsx

--- Layouts/Footer.tsx
+++ Layouts/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className='w-full mt-[5rem] bg-gray-300 flex justify-center items-center flex-col'>
+        <footer className='w-full mt-10 flex justify-center items-center flex-col'>
             <div className="py-10">Hot gist. All rights reserved</div>
             <p className="text-xl mt-4">Benedict Ughulu</p>
         </footer>
